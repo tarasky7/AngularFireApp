fb2fb0896c55402c827c9703f2759106c86bbdd2
wzy <wzypku86@gmail.com>
2020-06-02 20:10:40 -0400

    set up for Angularfire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDDLgZKJ38t9_uoc4t6acBzHBJt2pZ7ojk",
+    authDomain: "pencil-intern-app.firebaseapp.com",
+    databaseURL: "https://pencil-intern-app.firebaseio.com",
+    projectId: "pencil-intern-app",
+    storageBucket: "pencil-intern-app.appspot.com",
+    messagingSenderId: "1061634086215",
+    appId: "1:1061634086215:web:cd98c051d8f847263e6862",
+    measurementId: "G-N9V6K0N018"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDDLgZKJ38t9_uoc4t6acBzHBJt2pZ7ojk",
+    authDomain: "pencil-intern-app.firebaseapp.com",
+    databaseURL: "https://pencil-intern-app.firebaseio.com",
+    projectId: "pencil-intern-app",
+    storageBucket: "pencil-intern-app.appspot.com",
+    messagingSenderId: "1061634086215",
+    appId: "1:1061634086215:web:cd98c051d8f847263e6862",
+    measurementId: "G-N9V6K0N018"
+  }
 };
 
 /*
